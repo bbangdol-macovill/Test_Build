3721b59280ed6cd77d2170c2a2abc57274ded5a6
bbangdol <bbangdol@macovill.com>
2022-09-21 11:25:10 +0900

    git 테스트중.

--- Tools/PushAssetBundleSetting.bat
+++ Tools/PushAssetBundleSetting.bat
@@ -1,1 +1,19 @@
 @echo off
+
+rem git add
+git add ../Assets/AddressableAssetsData/*
+
+echo ADD
+pause > nul
+
+rem git commit
+git commit -m JenkinsBuild(develop)
+
+echo Commit
+pause > nul
+
+rem git push
+git push origin develop
+
+echo Push
+pause > nul
